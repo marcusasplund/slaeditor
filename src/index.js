89ce3660b1298b20e0bee8dd035c18be412e2881
Marcus Asplund <marcus.asplund@avinode.com>
2020-12-22 16:48:36 +0100

    editor

--- src/index.js
+++ src/index.js
@@ -1,159 +1,249 @@
-/* global btoa */
-/** @jsx h */
-import { h, app } from 'hyperapp'
-import { targetValue } from '@hyperapp/events'
+/* global btoa, localStorage */
+import { h, text, app } from 'hyperapp'
 import './styles/app.scss'
 
-const example = `<html>
+const storageKey = 'editor'
+
+const saveToLocalStorage = (state) => localStorage.setItem(storageKey, JSON.stringify(state))
+
+const getFromLocalStorage = () => JSON.parse(localStorage.getItem(storageKey))
+
+const exampleCSS = ''
+
+const exampleHTML = '<div id="app"></div>'
+
+const exampleJS = `
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      text: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text) {
+       return false;
+    }
+
+    this.setState(prevState => ({
+      list: prevState.list.concat(this.state.text),
+      text: ''
+    }))
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  removeItem(item) {
+    const newList = this.state.list.filter(text => text !== item)
+    this.setState({ list: newList})
+  }
+
+  render() {
+    return (
+      <div>
+       <pre><code>{JSON.stringify(this.state)}</code></pre>
+        <h1>Todo list</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.text} onChange={e => this.handleChange(e)} />
+          <button>Add</button>
+          <table>
+            {this.state.list.map(item => {
+              return (
+                <tr key={item}>
+                  <td>{item}</td>
+                  <td><button onClick={() => this.removeItem(item)}>Delete</button></td>
+                </tr>)
+            })}
+          </table>
+        </form>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Todo />, document.getElementById('app'));`
+
+const code = (style, html, js) => `
+<html>
   <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossorigin="anonymous" />
     <style>
-    html {
-      line-height: 1;
-    }
-    body {
-      align-items: center;
-      background-color: #111;
-      display: flex;
-      font-family: Helvetica Neue, sans-serif;
-      height: 100vh;
-      justify-content: center;
-      margin: 0;
-      padding: 0;
-      text-align: center;
-    }
-    h1 {
-      color: #00caff;
-      font-weight: 100;
-      font-size: 8em;
-      margin: 0;
-      padding-bottom: 15px;
-    }
-    button {
-      background: #111;
-      border-radius: 0px;
-      border: 1px solid #00caff;
-      color: #00caff;
-      font-size: 2em;
-      font-weight: 100;
-      margin: 0;
-      outline: none;
-      padding: 5px 15px;
-      transition: background .2s;
-    }
-    button:hover, button:active, button:disabled {
-      background: #00caff;
-      color: #111;
-    }
-    button:active {
-      outline: 2px solid #00caff;
-    }
-    button:focus {
-      border: 1px solid #00caff;
-    }
-    button + button {
-      margin-left: 3px;
-    }
+    ${style}
     </style>
     </head>
     <body>
-      <div id="app"></div>
-      <script type="module">
-        import { h, app } from "https://unpkg.com/hyperapp"
-
-        app({
-          init: 0,
-          view: state =>
-            h("div", {}, [
-              h("h1", {}, state),
-              h("button", { onClick: state => state - 1 }, "-"),
-              h("button", { onClick: state => state + 1 }, "+")
-            ]),
-          node: document.getElementById("app")
-        })
+    ${html}
+    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/babel-standalone/babel.min.js"></script>
+    <script type="text/babel">
+      ${js}
       </script>
   </body>
 </html>`
 
-const initialState = {
-  parsed: '',
-  code: '',
-  copied: false,
-  example: example,
-  placeholder: 'Paste your awesome website/app code here'
+const initialState = getFromLocalStorage() || {
+  parsed: btoa(code(exampleCSS, exampleHTML, exampleJS)),
+  html: exampleHTML,
+  css: exampleCSS,
+  js: exampleJS,
+  checked: 'js'
 }
 
-const ParseString = (state, value) => ({
-  ...state,
-  copied: false,
-  parsed: btoa(value)
-})
+const SetHTML = (state, event) => {
+  const newHTML = event.target.value
+  const newCode = code(state.css, newHTML, state.js)
+  const newState = {
+    ...state,
+    html: newHTML,
+    checked: 'html',
+    parsed: btoa(newCode)
+  }
+  saveToLocalStorage(newState)
+  return newState
+}
 
-const CopyExampleCode = state => ({
-  ...state,
-  copied: true,
-  code: example,
-  parsed: btoa(example)
-})
+const SetCSS = (state, event) => {
+  const newCSS = event.target.value
+  const newCode = code(newCSS, state.html, state.js)
+  const newState = {
+    ...state,
+    css: newCSS,
+    checked: 'css',
+    parsed: btoa(newCode)
+  }
+  saveToLocalStorage(newState)
+  return newState
+}
 
-const TextArea = ({ state }) => (
-  <textarea
-    id='codearea'
-    onInput={[ParseString, targetValue]}
-    placeholder={state.placeholder}
-  >
-    {state.code}
-  </textarea>
-)
+const SetJS = (state, event) => {
+  const newJS = event.target.value
+  const newCode = code(state.css, state.html, newJS)
+  const newState = {
+    ...state,
+    js: newJS,
+    checked: 'js',
+    parsed: btoa(newCode)
+  }
+  saveToLocalStorage(newState)
+  return newState
+}
 
-const Result = ({ state }) => (
-  <section>
-    <a href={`data:text/html;base64, ${state.parsed}`}>
-      <p>
-      In some browsers you can right click this link and open in a new tab*<br />
-      </p>
-    </a>
-    <p>
-      <small>*Due to browser restrictions it does not work to open such a link in the same window</small>
-    </p>
-    <hr />
-    <p>
-    Below is your app as a base64-encoded link that you can copy and paste in your browsers adress bar
-      <br />
-    </p>
-    <pre>
-      <code>
-    data:text/html;base64, {state.parsed}
-      </code>
-    </pre>
-    <hr />
-  </section>
-)
+const SetChecked = (state, event) => {
+  const newState = {
+    ...state,
+    checked: event.target.id
+  }
+  saveToLocalStorage(newState)
+  return newState
+}
 
-const Test = ({ state }) => (
-  <section>
-    <p>
-    To try it out; copy the example code below, paste in the left pane, then copy the base64 result produced above and paste in a browser address bar
-      <br />
-      <button onClick={CopyExampleCode}>copy example to textarea</button>
-    </p>
-    <pre>
-      <code>
-        {state.example}
-      </code>
-    </pre>
-  </section>
-)
+const HTMLArea = (state) =>
+  h('textarea', {
+    oninput: SetHTML,
+    placeholder: 'Put your HTML here'
+  }, text(state.html))
+
+const CSSArea = (state) =>
+  h('textarea', {
+    oninput: SetCSS,
+    placeholder: 'Put your CSS here'
+  }, text(state.css))
+
+const JSArea = (state) =>
+  h('textarea', {
+    oninput: SetJS,
+    placeholder: 'Put your JS here'
+  }, text(state.js))
+
+const Result = (state) =>
+  h('section', {}, [
+    h('iframe', {
+      src: `data:text/html;base64, ${state.parsed}`
+    })
+  ])
 
 app({
   init: initialState,
-  view: state => (
-    <div id='editor'>
-      <TextArea state={state} />
-      <div>
-        {state.parsed ? <Result state={state} /> : ''}
-        {state.copied ? '' : <Test state={state} />}
-      </div>
-      <p>You can look at the code <a href='https://github.com/marcusasplund/slaeditor'>here</a></p>
-    </div>),
+  view: state =>
+    h('div', {
+      id: 'editor'
+    }, [
+      h('div', {
+        id: 'left-panel',
+        class: 'tabs'
+      }, [
+        h('div', {
+          class: 'tab'
+        }, [
+          h('input', {
+            type: 'radio',
+            id: 'html',
+            name: 'tab-group-1',
+            onclick: SetChecked,
+            checked: state.checked === 'html'
+          }),
+          h('label', {
+            for: 'html'
+          }, text('html')),
+          h('div', {
+            class: 'content'
+          }, HTMLArea(state))
+        ]),
+        h('div', {
+          class: 'tab'
+        }, [
+          h('input', {
+            type: 'radio',
+            id: 'css',
+            name: 'tab-group-1',
+            onclick: SetChecked,
+            checked: state.checked === 'css'
+          }),
+          h('label', {
+            for: 'css'
+          }, text('css')),
+          h('div', {
+            class: 'content'
+          }, [
+            CSSArea(state)
+          ])
+        ]),
+        h('div', {
+          class: 'tab'
+        }, [
+          h('input', {
+            type: 'radio',
+            id: 'js',
+            onclick: SetChecked,
+            name: 'tab-group-1',
+            checked: state.checked === 'js'
+          }),
+          h('label', {
+            for: 'js'
+          }, text('javascript')),
+          h('div', {
+            class: 'content'
+          }, [
+            JSArea(state)
+          ])
+        ])
+      ]),
+      h('div', {
+        id: 'right-panel'
+      }, [
+        Result(state)
+      ])
+    ]),
   node: document.getElementById('app')
 })
